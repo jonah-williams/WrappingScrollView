583af39e80b8aa8aba7c87db69663298e3704c27
Jonah Williams <jonah@carbonfive.com>
2010-08-22 10:54:09 -0700

    correct copyright, this project was developed independently and is not owned by Carbon Five

--- Classes/WrappingScrollView.m
+++ Classes/WrappingScrollView.m
@@ -3,7 +3,7 @@
 //  WrappingScrollView
 //
 //  Created by Jonah Williams on 8/20/10.
-//  Copyright 2010 Carbon Five. All rights reserved.
+//  Copyright 2010 Jonah Williams. All rights reserved.
 //
 
 #import <QuartzCore/QuartzCore.h>
